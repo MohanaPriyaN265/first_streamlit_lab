f28ca4d663e7b22983b2c61a209661bcc7a119b1
MohanaPriyaN265 <80239285+MohanaPriyaN265@users.noreply.github.com>
2022-07-09 17:57:32 +0530

    Update streamlit_app.py
    adding snowflake code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-
 import streamlit
 import pandas
 
@@ -23,7 +22,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show )
 
 #new section to display fruitvice api response
-
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
@@ -31,11 +29,17 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-
 # normalise the json version of the response 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it as the table
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
